f90ca7a19559c80ecfa76011f557fef1c0862328
LeBlogModernWorkplace <149409622+LeblogModernWorkplace@users.noreply.github.com>
2025-09-30 12:29:41 +0200

    Update GetHPBiosPwdState.ps1

--- CheckHPBiosPwdSet/GetHPBiosPwdState.ps1
+++ CheckHPBiosPwdSet/GetHPBiosPwdState.ps1
@@ -1,1 +1,28 @@
+<#
+   .SYNOPSIS
+ Check HP Bios Password is Set
 
+#>
+
+# Required Value
+
+$ExpectedStatus = "True"
+
+
+# Get the HP Bios password statut with WMI query
+$CurrentStatus = Get-wmiobject -namespace root\hp\instrumentedbios -class HP_BiosSetting | Where-object name -EQ "Setup Password" | Select -expandProperty active
+
+# Check the result and return the statut
+$ReturnStatus = if ($ExpectedStatus -ne $CurrentStatus) {
+    $false
+    Write-Host "Bios Pwd not OK"
+} else {
+    $true
+    Write-Host "Bios Pwd OK"
+}
+
+# Send the output for Json check
+$output = @{
+    HPBiosBiospwdStatus = $ReturnStatus
+    }
+ return $output | ConvertTo-Json -Compress
